a3e79d56b19218a111a3c783d08e04d3bef1e247
mSkaarup <36762502+mSkaarup@users.noreply.github.com>
2018-02-23 06:59:45 -0800

    Add files via upload

---
+++ C#/WeightedRandomException.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+
+namespace WeightedRandom
+{
+	[Serializable]
+	public class WeightedRandomException : Exception 
+	{
+		public WeightedRandomException()
+		{
+		}
+
+		public WeightedRandomException(string message) 
+			: base(message)
+		{
+		}
+
+		public WeightedRandomException(string message, Exception inner) 
+			: base(message, inner)
+		{
+		}
+	}
+}
